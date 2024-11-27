5c6981de41a9aa8794cd6eec1975820087c410d2
Felipe Santiago Morais <contatofelipesantiago@gmail.com>
2024-11-27 01:10:34 -0300

    feat(app): configure fastify type provider zod

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,17 @@
-import { PrismaClient } from "@prisma/client";
+import fastifyCors from "@fastify/cors";
 import fastify from "fastify";
+import {
+    serializerCompiler,
+    validatorCompiler,
+    ZodTypeProvider,
+} from "fastify-type-provider-zod";
 
-export const app = fastify();
+export const app = fastify().withTypeProvider<ZodTypeProvider>();
 
-const prisma = new PrismaClient();
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(fastifyCors, {
+  origin: "*",
+  credentials: true,
+});
