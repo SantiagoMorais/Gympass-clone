c91abbbd96dcf68cd3e8ca4004ad08116a947226
Felipe Santiago Morais <fillikz@gmail.com>
2024-12-28 18:21:39 -0300

    feat(get-distances-between-coordinates): change function not using geolib

--- src/utils/get-distance-between-coordinates.ts
+++ src/utils/get-distance-between-coordinates.ts
@@ -5,10 +5,28 @@ export const getDistanceBetweenCoordinates = (
   from: ICoordinate,
   to: ICoordinate
 ) => {
-  const distanceInMeters = getDistance(
-    { latitude: from.latitude, longitude: from.longitude },
-    { latitude: to.latitude, longitude: to.longitude }
-  );
+  if (from.latitude === to.latitude && from.longitude === to.longitude) {
+    return 0
+  }
 
-  return distanceInMeters;
+  const fromRadian = (Math.PI * from.latitude) / 180
+  const toRadian = (Math.PI * to.latitude) / 180
+
+  const theta = from.longitude - to.longitude
+  const radTheta = (Math.PI * theta) / 180
+
+  let dist =
+    Math.sin(fromRadian) * Math.sin(toRadian) +
+    Math.cos(fromRadian) * Math.cos(toRadian) * Math.cos(radTheta)
+
+  if (dist > 1) {
+    dist = 1
+  }
+
+  dist = Math.acos(dist)
+  dist = (dist * 180) / Math.PI
+  dist = dist * 60 * 1.1515
+  dist = dist * 1.609344
+
+  return dist
 };
