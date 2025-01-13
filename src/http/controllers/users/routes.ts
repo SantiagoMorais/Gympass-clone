c8011dce3c202b3c4d35b7e987bb3c37ec198e8b
Felipe Santiago Morais <fillikz@gmail.com>
2025-01-13 10:43:31 -0300

    feat(refreshToken): implement refresh token functionality with tests

--- src/http/controllers/users/routes.ts
+++ src/http/controllers/users/routes.ts
@@ -3,10 +3,13 @@ import { authenticate } from "../users/authenticate";
 import { register } from "../users/register";
 import { profile } from "../users/profile";
 import { verifyJWT } from "http/middlewares/verify-jwt";
+import { refresh } from "./refresh";
 
 export const usersRoutes: FastifyPluginAsyncZod = async (app) => {
   app.post("/sessions", authenticate);
   app.post("/users", register);
+
+  app.patch("/token/refresh", refresh);
 
   // Authenticated routes
   app.get("/me", { onRequest: [verifyJWT] }, profile);
